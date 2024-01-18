bb1a919d8f0a39b8b61d3f66abaf08fef2112a1a
Ismail Chamadia edited <chamadia.ismail17683@gmail.com>
2024-01-18 10:03:27 +0530

    file added

---
+++ sub.java
@@ -0,0 +1,10 @@
+public class SubOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sub;  
+sub = n1 - n2;  
+System.out.println("The sub of numbers is: "+sub);  
+System.out.println("By Ismail Chamadia");
+}  
+} 
