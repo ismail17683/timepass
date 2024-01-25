08004f28a87352b6b918a819e8c24fa65b49ca40
Ismail Chamadia edited <chamadia.ismail17683@gmail.com>
2024-01-25 09:23:40 +0530

    updated

--- sum.java
+++ sum.java
@@ -1,10 +1,10 @@
-public class SumOfNumbers1  
+public class SubOfNumbers1  
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
+int n1 = 225, n2 = 115, sub;  
+sub = n1 - n2;  
+System.out.println("The sub of numbers is: "+sub);  
 System.out.println("By Ismail Chamadia");
 }  
 } 
