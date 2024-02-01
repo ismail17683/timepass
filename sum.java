86009b6f9ba54a7969d1dae06957e86e18b8be3f
Ismail Chamadia edited <chamadia.ismail17683@gmail.com>
2024-02-01 09:05:51 +0530

    changed

--- sum.java
+++ sum.java
@@ -6,5 +6,6 @@ int n1 = 225, n2 = 115, sub;
 sub = n1 - n2;  
 System.out.println("The sub of numbers is: "+sub);  
 System.out.println("By Ismail Chamadia");
+System.out.println("File is changed");
 }  
 } 
